61c9015d7f178acb132ce3e4f503489acc32f4b0
DeadLiveMan <deadliveman2@gmail.com>
2018-09-09 04:14:46 +0300

    worked wd_ps2 - not complete

--- wd_ps2/task/js/script.js
+++ wd_ps2/task/js/script.js
@@ -1,59 +1,107 @@
 
-function task_1() {
+function sum_1() {
     let sum = 0;
-    for (let i = -1000; i <= 1000; i++) {
+    let sum1 = document.getElementById("task_1_start").value;
+    let sum2 = document.getElementById("task_1_end").value;
+
+    // Check the numbers
+    if (!isNumeric(sum1) || !isNumeric(sum2)) {
+        document.getElementById("sum_1").innerHTML = "Wrong input <br>";
+        document.getElementById("sum_1").style.color = "darkOrange";
+        return;
+    }
+    const minValue = -1000;
+    const maxValue = 1000;
+
+    sum1 = Number(sum1);
+    sum2 = Number(sum2);
+
+    if (sum1 < minValue || sum2 > maxValue || sum1 > sum2) {
+        document.getElementById("sum_1").innerHTML = "Index can be -1000 to 1000 <br> and start index <= end index <br>";
+        document.getElementById("sum_1").style.color = "darkOrange";
+        return;
+    }
+
+    for (let i = sum1; i <= sum2; i++) {
         sum += i;
     }
-    document.write("sum = " + sum);
+    document.getElementById("sum_1").style.color = "black";
+    document.getElementById("sum_1").innerHTML = sum + "<br>";
 }
 
-function task_2() {
+function sum_2() {
     let sum = 0;
-    for (let i = -1000; i <= 1000; i++) {
-        if (i % 10 === 2 || i % 10 === 3 || i % 10 === 7) {
+    let sum1 = Number(document.getElementById("task_2_start").value);
+    let sum2 = Number(document.getElementById("task_2_end").value);
+    let positions = document.getElementById("positions").value;
+
+    if (!isNumericWithComma(positions)) {
+        document.getElementById("sum_2").innerHTML = "Incorrect positions <br>";
+        document.getElementById("sum_2").style.color = "darkOrange";
+    }
+
+    const minValue = -1000;
+    const maxValue = 1000;
+
+    sum1 = Number(sum1);
+    sum2 = Number(sum2);
+
+    if (sum1 < minValue || sum2 > maxValue || sum1 > sum2) {
+        document.getElementById("sum_1").innerHTML = "Index can be -1000 to 1000 <br> and start index < end index <br>";
+        document.getElementById("sum_1").style.color = "darkOrange";
+        return;
+    }
+
+    //get positions in massive
+    let positionArray = positions.split(",");
+
+    for (let i = sum1; i <= sum2; i++) {
+        if (contains(positionArray, i % 10)) {
             sum += i;
         }
     }
-    document.write("sum = " + sum);
+    document.getElementById("sum_2").innerHTML = sum + "";
 }
 
-function task_3() {
-    let result;
-    for (let i = 1; i <= 50; i++) {
+function stars_50() {
+    let result = "";
+    let stars = Number(document.getElementById("task_3_stars").value);
+    for (let i = 1; i <= stars; i++) {
         for(let j = 0; j < i; j++) {
             result += "*";
         }
         result += "<br/>";
     }
-    return result;
+    document.getElementById("stars_50").innerHTML = result;
 }
 
-function task_4(second) {
+function getFullTime(second) {
     const secondsInHour = 3600;
     const secondsInMinutes = 60;
     const decimal = 10;
+    let sec = second.value;
 
     let hour = 0;
     let minutes = 0;
 
-    while (second >= secondsInHour) {
+    while (sec >= secondsInHour) {
         hour++;
-        second -= secondsInHour;
+        sec -= secondsInHour;
     }
 
-    while (second >= secondsInMinutes) {
+    while (sec >= secondsInMinutes) {
         minutes++;
-        second -= secondsInMinutes;
+        sec -= secondsInMinutes;
     }
 
     if (hour < decimal)
         hour = "0" + hour;
     if (minutes < decimal)
         minutes = "0" + minutes;
-    if (second < decimal)
-        second = "0" + second;
+    if (sec < decimal)
+        sec = "0" + sec;
 
-    document.write(hour + ":" + minutes + ":" + second);
+    document.getElementById("full_time").innerHTML = "Result: " + (hour + ":" + minutes + ":" + sec);
 }
 
 function task_5(years) {
@@ -103,7 +151,7 @@ function task_10(number) {
         number -= number % 10;
         number /= 10;
     }
-    document.write(sum);
+    document.write(sum + "");
 }
 
 function task_11() {
@@ -115,5 +163,21 @@ function test() {
         task_5(i);
         document.write("<br/>")
     }
+}
+
+function isNumeric(value) {
+    return value.match(/^[-]?[0-9]*[0-9]$/);
+}
+
+function isNumericWithComma(value) {
+    return value.match(/^[0-9]([,][0-9])*$/);
+}
+
+function contains(arr, value) {
+    for (let i = 0; i < arr.length; i++) {
+        if (Number(arr[i]) === value)
+            return true;
+    }
+    return false;
 }
 
