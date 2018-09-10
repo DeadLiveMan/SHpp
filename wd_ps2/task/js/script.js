01aec4e584ef18265759ddfe4a145244d91f436a
DeadLiveMan <deadliveman2@gmail.com>
2018-09-11 00:20:48 +0300

    wd_ps2 in progress, not complete

--- wd_ps2/task/js/script.js
+++ wd_ps2/task/js/script.js
@@ -1,39 +1,41 @@
 
-function sumFirstTask() {
-    let sum = 0;
-    let sum1 = document.getElementById("task_1_start").value;
-    let sum2 = document.getElementById("task_1_end").value;
+/** Task 1 */
+function sumFirstTask(firstElement, secondElement) {
+    let result = 0;
+    firstElement = firstElement.value;
+    secondElement = secondElement.value;
     let element = document.getElementById("sum_1");
     const minValue = -1000;
     const maxValue = 1000;
 
     // Check the numbers
-    if (!isNumeric(sum1) || !isNumeric(sum2)) {
-        document.getElementById("sum_1").innerHTML = "Wrong input <br>";
-        document.getElementById("sum_1").style.color = "darkOrange";
+    if (!isNumeric(firstElement) || !isNumeric(secondElement)) {
+        element.innerHTML = "Wrong input <br>";
+        element.style.color = "darkOrange";
         return;
     }
     // convert to number
-    sum1 = Number(sum1);
-    sum2 = Number(sum2);
+    firstElement = Number(firstElement);
+    secondElement = Number(secondElement);
     // validate
-    if (sum1 < minValue || sum2 > maxValue || sum1 > sum2) {
+    if (firstElement < minValue || secondElement > maxValue || firstElement > secondElement) {
         element.innerHTML = "Index can be -1000 to 1000 <br> and start index <= end index <br>";
         element.style.color = "darkOrange";
         return;
     }
-    for (let i = sum1; i <= sum2; i++) {
-        sum += i;
+    for (let i = firstElement; i <= secondElement; i++) {
+        result += i;
     }
     element.style.color = "black";
-    element.innerHTML = sum + "<br>";
+    element.innerHTML = result + "<br>";
 }
 
-function sumWithFilter() {
-    let sum = 0;
-    let sum1 = document.getElementById("task_2_start").value;
-    let sum2 = document.getElementById("task_2_end").value;
-    let positions = document.getElementById("positions").value;
+/** Task 2 */
+function sumWithFilter(firstElement, secondElement, positions) {
+    let result = 0;
+    firstElement = firstElement.value;
+    secondElement = secondElement.value;
+    positions = positions.value;
     let element = document.getElementById("sum_2");
     const minValue = -1000;
     const maxValue = 1000;
@@ -41,74 +43,102 @@ function sumWithFilter() {
     element.style.color = "darkOrange";
 
     // Check the numbers
-    if (!isNumeric(sum1) || !isNumeric(sum2)) {
-        document.getElementById("sum_1").innerHTML = "Wrong input <br>";
-        document.getElementById("sum_1").style.color = "darkOrange";
+    if (!isNumeric(firstElement) || !isNumeric(secondElement)) {
+        element.innerHTML = "Wrong input <br>";
+        element.style.color = "darkOrange";
         return;
     }
 
-    if (!isNumericWithComma(positions)) {
+    firstElement = Number(firstElement);
+    secondElement = Number(secondElement);
+
+    if (positions !== "" && !isNumericWithComma(positions)) {
         element.innerHTML = "Incorrect positions <br>";
         return;
     }
 
-    sum1 = Number(sum1);
-    sum2 = Number(sum2);
-
-    if (sum1 < minValue || sum2 > maxValue || sum1 > sum2) {
+    if (firstElement < minValue || secondElement > maxValue || firstElement > secondElement) {
         element.innerHTML = "Index can be -1000 to 1000 <br> and start index < end index <br>";
         return;
     }
-    element.style.color = "black";
 
     //get positions in massive
-    let positionArray = positions.split(",");
+    let positionArray = "";
+    if (positions !== "") {
+        positionArray = positions.split(",");
+    }
 
-    for (let i = sum1; i <= sum2; i++) {
-        if (contains(positionArray, i % 10)) {
-            sum += i;
+    for (let i = firstElement; i <= secondElement; i++) {
+        if (positionArray === "" || contains(positionArray, Math.abs(i % 10))) {
+            result += i;
         }
     }
-    element.innerHTML = sum + "";
+    element.style.color = "black";
+    element.innerHTML = result + "";
 }
 
-function printStars() {
-    let result = "";
-    let stars = Number(document.getElementById("task_3_stars").value);
+/** Task 3 */
+function printStars(countStars) {
+    countStars = countStars.value;
     let element = document.getElementById("stars_50");
-    for (let i = 1; i <= stars; i++) {
+    // Check the numbers
+    if (!isNumericPositive(countStars)) {
+        element.innerHTML = "Wrong input <br>";
+        element.style.color = "darkOrange";
+        return;
+    }
+    let result = "";
+    countStars = Number(countStars);
+    for (let i = 1; i <= countStars; i++) {
         for(let j = 0; j < i; j++)
             result += "*";
         result += "<br/>";
     }
+    element.style.color = "black";
     element.innerHTML = result;
 }
 
+/** Task 4 */
 function getDateTime(second) {
     const secondsInHour = 3600;
     const secondsInMinutes = 60;
     const decimal = 10;
-    let sec = second.value;
     let element = document.getElementById("full_time");
+    second = second.value;
 
-    let hour = Math.floor(sec / secondsInHour);
-    sec -= secondsInHour * hour;
-    let minutes = Math.floor(sec / secondsInMinutes);
-    sec -= secondsInMinutes * minutes;
+    if (!isNumericPositive(second)) {
+        element.innerHTML = "Wrong input <br>";
+        element.style.color = "darkOrange";
+        return;
+    }
+
+    let hour = Math.floor(second / secondsInHour);
+    second -= secondsInHour * hour;
+    let minutes = Math.floor(second / secondsInMinutes);
+    second -= secondsInMinutes * minutes;
 
     if (hour < decimal)
         hour = "0" + hour;
     if (minutes < decimal)
         minutes = "0" + minutes;
-    if (sec < decimal)
-        sec = "0" + sec;
+    if (second < decimal)
+        second = "0" + second;
 
-    element.innerHTML = hour + ":" + minutes + ":" + sec;
+    element.style.color = "black";
+    element.innerHTML = hour + ":" + minutes + ":" + second;
 }
 
+/** Task 5 */
 function printAge(years) {
-    years = Number(years.value);
+    years = years.value;
     let element = document.getElementById("age");
+    if (!isNumericPositive(years)) {
+        element.innerHTML = "Wrong input <br>";
+        element.style.color = "darkOrange";
+        return;
+    }
+
+    years = Number(years);
     let fraze = "лет";
     if (years < 1 || years > 110)
         fraze = "бред";
@@ -118,10 +148,44 @@ function printAge(years) {
         fraze = "года";
     if (years > 10 && years < 15)
         fraze = "лет";
+    element.style.color = "black";
     element.innerHTML = (years + " " + fraze);
 }
 
 function intervalDate(firstData, secondData) {
+
+    let mounts = [  "january",  "february", "march",        "april",    "may",      "june",
+                    "july",     "august",   "september",    "october",  "november", "december"];
+    let daysInMounts = [31,28,31,30,31,30,31,31,30,31,30,31];
+
+    firstData = firstData.value;
+    secondData = secondData.value;
+
+    /**todo checks*/
+
+    // remove duplicate space
+    firstData = firstData.replace(","," ");
+    secondData = secondData.replace(","," ");
+    while (firstData.match(/\s{2,}/) || secondData.match(/\s{2,}/)) {
+        firstData = firstData.replace(/\s{2,}/, " ");
+        secondData = secondData.replace(/\s{2,}/, " ");
+    }
+
+    // create massive
+    firstData = firstData.split(" ");
+    secondData = secondData.split(" ");
+
+    // get date
+    let startMount = mounts.indexOf(firstData[0].toLowerCase()) + 1;
+    let endMount = mounts.indexOf(secondData[0]) + 1;
+    let startDay = Number(firstData[1]);
+    let endDay = Number(secondData[1]);
+    let startYear = Number(firstData[2]);
+    let endYear = Number(secondData[2]);
+
+    let passedYear = endYear - startYear;
+
+    alert(passedYear + " -- " + startYear);
 
     firstData = firstData.value;
     secondData = secondData.value;
@@ -180,6 +244,10 @@ function test() {
 
 function isNumeric(value) {
     return value.match(/^[-]?[0-9]*[0-9]$/);
+}
+
+function isNumericPositive(value) {
+    return value.match(/^[0-9]*[0-9]$/);
 }
 
 function isNumericWithComma(value) {
