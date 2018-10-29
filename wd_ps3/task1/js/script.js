a5a07fda9bb0b9343d5df941d9a583d567c4119c
DeadLiveMan <deadliveman2@gmail.com>
2018-10-29 18:23:03 +0200

    edit ed_ps3

--- wd_ps3/task1/js/script.js
+++ wd_ps3/task1/js/script.js
@@ -1,55 +1,60 @@
+$(document).ready( function () {
+    const element = ['.content', '.menu', '.footer'];
+    const $scrollButton = $('.scroll-button');
+    const $allDocument = $('*');
 
-const element = ['.content', '.menu', '.footer'];
-const $scrollButton = $('.scroll-button');
-const $allDocument = $('*');
+    const positionShowButton = 200;
+    const timeHideButton = 150;
+    const timeShowButton = 300;
 
-const scrollShowPosition = 300;
-const timeHide = 150;
-const timeShow = 300;
-const maxOpacity = 'show';
-const minOpacity = 'hide';
-
-let isAnimated = false;
-let isAnimatedDocument = false;
+    let isAnimatedButton = false;
+    let isAnimatedDocument = false;
 
 // stop animation after scroll wheel
-$allDocument.on('mousewheel', function () {
-    if(isAnimatedDocument) {
-        $allDocument.stop();
-        isAnimatedDocument = false;
-    }
-});
+    $allDocument.on('mousewheel', function () {
+        if (isAnimatedDocument) {
+            $allDocument.stop();
+            isAnimatedDocument = false;
+        }
+    });
 
 // event for click header links
-$(".header__link_item").click(function() {
-    isAnimatedDocument = true;
-    const linkClass = element[$(this).index()];
-    $allDocument.animate( {
-        scrollTop: $(linkClass).offset().top + $(linkClass).height() / 2 - $(window).height() / 2
-        }, 500, function () {isAnimatedDocument = false;});
-});
+    $(".header__link_item").click(function () {
+        isAnimatedDocument = true;
+        const linkClass = element[$(this).index()];
+        $allDocument.animate({
+            scrollTop: $(linkClass).offset().top + $(linkClass).height() / 2 - $(window).height() / 2
+        }, 500, function () {
+            isAnimatedDocument = false;
+        });
+    });
 
 // event for click scroll button
-$scrollButton.click(function () {
-    if(!isAnimatedDocument) {
-        isAnimatedDocument = true;
-        $allDocument.animate({scrollTop: 0}, 800);
-    }
-});
+    $scrollButton.click(function () {
+        if (!isAnimatedDocument) {
+            isAnimatedDocument = true;
+            $allDocument.animate({scrollTop: 0}, 800);
+        }
+    });
 
 // hide and show scroll button
-$(window).scroll(function () {
-    if(($(window).scrollTop()) > scrollShowPosition && !isAnimated) {
-        isAnimated = true;
-        $scrollButton.animate({opacity: maxOpacity}, timeShow, function () {
-            if ($(window).scrollTop() < scrollShowPosition) {$scrollButton.animate({opacity: minOpacity}, timeHide)}
-            isAnimated = false;
-        });
-    } else if (!isAnimated){
-        isAnimated = true;
-        $scrollButton.animate({opacity: minOpacity}, timeHide, function () {
-            if ($(window).scrollTop() > scrollShowPosition) {$scrollButton.animate({opacity: maxOpacity}, timeShow)}
-            isAnimated = false;
-        });
-    }
+    $(window).scroll(function () {
+        if (($(window).scrollTop()) > positionShowButton && !isAnimatedButton) {
+            isAnimatedButton = true;
+            $scrollButton.fadeIn(timeShowButton, function () {
+                if ($(window).scrollTop() < positionShowButton) {
+                    $scrollButton.fadeOut(timeHideButton);
+                }
+                isAnimatedButton = false;
+            });
+        } else if (!isAnimatedButton) {
+            isAnimatedButton = true;
+            $scrollButton.fadeOut(timeHideButton, function () {
+                if ($(window).scrollTop() > positionShowButton) {
+                    $scrollButton.fadeIn(timeShowButton);
+                }
+                isAnimatedButton = false;
+            });
+        }
+    });
 });
