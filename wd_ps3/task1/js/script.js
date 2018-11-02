440a21ebb03674841bf8cce3d9217d8a700072bc
DeadLiveMan <deadliveman2@gmail.com>
2018-11-02 20:03:50 +0200

    edit wd_ps3

--- wd_ps3/task1/js/script.js
+++ wd_ps3/task1/js/script.js
@@ -3,58 +3,46 @@
     const $scrollButton = $('.scroll-button');
     const $allDocument = $('html, body');
 
-
     const positionShowButton = 200;
     const timeHideButton = 150;
     const timeShowButton = 300;
 
-    let isAnimatedButton = false;
-    let isAnimatedDocument = false;
-
 // stop animation after scroll wheel
     $allDocument.on('mousewheel', function () {
-        if (isAnimatedDocument) {
+        if ($allDocument.animate.length) {
             $allDocument.stop();
-            isAnimatedDocument = false;
+            //isAnimatedDocument = false;
         }
     });
 
 // event for click header links
     $('.header__link_item').on('click', function () {
-        isAnimatedDocument = true;
         const linkClass = element[$(this).index()];
         $allDocument.animate({
             scrollTop: $(linkClass).offset().top + $(linkClass).height() / 2 - $(window).height() / 2
-        }, 500, function () {
-            isAnimatedDocument = false;
-        });
+        }, 500);
     });
 
 // event for click scroll button
     $scrollButton.on('click', function () {
-        if (!isAnimatedDocument) {
-            isAnimatedDocument = true;
+        if (!$('.html:animated').length) {
             $allDocument.animate({scrollTop: 0}, 800);
         }
     });
 
 // hide and show scroll button
     $(window).on('scroll', function () {
-        if (($(window).scrollTop()) > positionShowButton && !isAnimatedButton) {
-            isAnimatedButton = true;
+        if (!$('.scroll-button:animated').length && ($(window).scrollTop()) > positionShowButton) {
             $scrollButton.fadeIn(timeShowButton, function () {
                 if ($(window).scrollTop() < positionShowButton) {
                     $scrollButton.fadeOut(timeHideButton);
                 }
-                isAnimatedButton = false;
             });
-        } else if (!isAnimatedButton) {
-            isAnimatedButton = true;
+        } else if (!$('.scroll-button:animated').length) {
             $scrollButton.fadeOut(timeHideButton, function () {
                 if ($(window).scrollTop() > positionShowButton) {
                     $scrollButton.fadeIn(timeShowButton);
                 }
-                isAnimatedButton = false;
             });
         }
     });
