79c44a20857e8f6595eed841525788a3aab63b1d
DeadLiveMan <deadliveman2@gmail.com>
2018-11-20 17:37:28 +0200

    edit wd_st1, replace all quotes

--- wd_st1/atm-template.js
+++ wd_st1/atm-template.js
@@ -8,13 +8,13 @@ const ATM = {
     cash: 2000,
     // all available users
     users: [
-        {number: "0000", pin: "000", debet: 0, type: "admin"}, // EXTENDED
-        {number: "0025", pin: "123", debet: 675, type: "user"}
+        {number: '0000', pin: '000', debet: 0, type: 'admin'}, // EXTENDED
+        {number: '0025', pin: '123', debet: 675, type: 'user'}
     ],
     // authorization
     auth: function(number, pin) {
         if (this.is_auth) {
-            console.log("please logout current user");
+            console.log('please logout current user');
             return;
         }
         for (let i = 0; i < this.users.length; i++) {
@@ -22,47 +22,47 @@ const ATM = {
                 this.is_auth = true;
                 this.current_user = this.users[i];
                 this.current_type = this.current_user.type;
-                console.log("authorization is ok ");
+                console.log('authorization is ok ');
                 this.history = 'authorization ' + this.current_type + ': ' + this.current_user.number;
                 return;
             }
         }
-        console.log("access denied");
+        console.log('access denied');
     },
     // check current debet
     check: function() {
         if (!this.is_auth) {
-            console.log("do authorization pls");
+            console.log('do authorization pls');
             return;
         }
         this.history = 'check debet ' + this.current_type + ': ' + this.current_user.number;
-        console.log("your debet is: " + this.current_user.debet);
+        console.log('your debet is: ' + this.current_user.debet);
     },
     // get cash - available for user only
     getCash: function(amount) {
         if (!this.is_auth) {
-            console.log("do authorization pls");
+            console.log('do authorization pls');
             return;
         }
 
         if (this.current_type === 'admin') {
-            console.log("sorry, its only for users");
+            console.log('sorry, its only for users');
             return;
         }
 
         amount = Number(amount);
 
         if (isNaN(amount) || amount < 1) {
-            console.log("incorrect input");
+            console.log('incorrect input');
             return;
         }
 
         if (amount > this.current_user.debet) {
-            console.log("you have only " + this.current_user.debet + " money");
+            console.log('you have only ' + this.current_user.debet + ' money');
             return;
         }
         if (amount > this.cash) {
-            console.log("ATM have only " + this.cash + " money");
+            console.log('ATM have only ' + this.cash + ' money');
             return;
         }
         this.cash -= amount;
@@ -74,19 +74,19 @@ const ATM = {
     // load cash - available for user only
     loadCash: function(amount){
         if (!this.is_auth) {
-            console.log("do authorization pls");
+            console.log('do authorization pls');
             return;
         }
 
         if (this.current_type === 'admin') {
-            console.log("sorry, its only for users");
+            console.log('sorry, its only for users');
             return;
         }
 
         amount = Number(amount);
 
         if (isNaN(amount) || amount < 1) {
-            console.log("incorrect input");
+            console.log('incorrect input');
             return;
         }
         this.cash += amount;
@@ -98,18 +98,18 @@ const ATM = {
     // load cash to ATM - available for admin only - EXTENDED
     load_cash: function(addition) {
         if (!this.is_auth) {
-            console.log("do authorization pls");
+            console.log('do authorization pls');
             return;
         }
         if (this.current_type !== 'admin') {
-            console.log("access denied");
+            console.log('access denied');
             return;
         }
 
         addition = Number(addition);
 
         if (isNaN(addition) || addition < 1) {
-            console.log("incorrect input");
+            console.log('incorrect input');
             return;
         }
 
@@ -120,14 +120,15 @@ const ATM = {
     // get report about cash actions - available for admin only - EXTENDED
     getReport: function() {
         if (!this.is_auth) {
-            console.log("do authorization pls");
+            console.log('do authorization pls');
             return;
         }
+
         if (this.current_type !== 'admin') {
-            console.log("access denied");
+            console.log('access denied');
             return;
         }
-        console.log("cash: " + this.cash + "\n" + this.history);
+        console.log('cash: ' + this.cash + '\n' + 'Last operation: ' + this.history);
         this.history = 'get Report ' + this.current_type + ': ' + this.current_user.number;
     },
     // log out
