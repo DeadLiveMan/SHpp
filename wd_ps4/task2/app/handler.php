688395e225cd1a536897c33c6f0a08b175a511bf
DeadLiveMan <deadliveman2@gmail.com>
2018-11-21 01:56:57 +0200

    wd_ps4 task2, add check for not existing value for vote

--- wd_ps4/task2/app/handler.php
+++ wd_ps4/task2/app/handler.php
@@ -2,14 +2,19 @@
 
 require '..'.DIRECTORY_SEPARATOR.'app'.DIRECTORY_SEPARATOR.'readStatistics.php';
 
-$index = $_POST["vote"];
-$fileName = '..'.DIRECTORY_SEPARATOR.'public'.DIRECTORY_SEPARATOR.'statistics.json';
 $statistics = getStatistics();
 
-$statistics -> $index++;
 
-$fw = fopen($fileName, 'w');
-fwrite($fw, json_encode($statistics));
-fclose($fw);
+
+if (isset($_POST["vote"]) && array_key_exists($_POST["vote"], $statistics)) {
+    $product = $_POST["vote"];
+    $fileName = '..'.DIRECTORY_SEPARATOR.'public'.DIRECTORY_SEPARATOR.'statistics.json';
+
+    $statistics -> $product++;
+
+    $fw = fopen($fileName, 'w');
+    fwrite($fw, json_encode($statistics));
+    fclose($fw);
+}
 
 header("location:..".DIRECTORY_SEPARATOR."public".DIRECTORY_SEPARATOR."piechart.php");
