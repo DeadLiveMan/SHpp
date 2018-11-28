f6a52982d75dda680cb71f378cedee57599247da
DeadLiveMan <deadliveman2@gmail.com>
2018-11-28 19:53:46 +0200

    add reports(history) in all conditions in methods and reformating

--- wd_st1/atm-template.js
+++ wd_st1/atm-template.js
@@ -2,7 +2,7 @@ const ATM = {
     is_auth: false, 
     current_user: false,
     current_type: false,
-    history: false,
+    history: ['ATM started ' + new Date().toUTCString()],
      
     // all cash of ATM
     cash: 2000,
@@ -14,7 +14,9 @@ const ATM = {
     // authorization
     auth: function(number, pin) {
         if (this.is_auth) {
-            console.log('please logout current user');
+            this.history.push(`auth: false, now authorized ${this.current_type}: ${this.current_user.number} `
+                + new Date().toUTCString());
+            console.log('please, logout current user');
             return;
         }
         for (let i = 0; i < this.users.length; i++) {
@@ -22,30 +24,36 @@ const ATM = {
                 this.is_auth = true;
                 this.current_user = this.users[i];
                 this.current_type = this.current_user.type;
-                console.log('authorization is ok ');
-                this.history = 'authorization ' + this.current_type + ': ' + this.current_user.number;
+                this.history.push(`auth: true, ${this.current_type}: ${this.current_user.number} `
+                    + new Date().toUTCString());
+                console.log('you authorized');
                 return;
             }
         }
-        console.log('access denied');
+        this.history.push(`auth: false, wrong pass/login ` + new Date().toUTCString());
+        console.log('Access denied, wrong pass/login');
     },
     // check current debet
     check: function() {
         if (!this.is_auth) {
-            console.log('do authorization pls');
+            this.history.push('check: false, not authorized ' + new Date().toUTCString());
+            console.log('Do authorization pls');
             return;
         }
-        this.history = 'check debet ' + this.current_type + ': ' + this.current_user.number;
-        console.log('your debet is: ' + this.current_user.debet);
+        this.history.push(`check: true, ${this.current_type}: ${this.current_user.number} ` + new Date().toUTCString());
+        console.log(`your debet is ${this.current_user.debet}`);
     },
     // get cash - available for user only
     getCash: function(amount) {
         if (!this.is_auth) {
-            console.log('do authorization pls');
+            this.history.push(`getCash: false, not authorized ` + new Date().toUTCString());
+            console.log('Do authorization pls');
             return;
         }
 
         if (this.current_type === 'admin') {
+            this.history.push(`getCash: false, it\`s not user ${this.current_type}: `
+                + `${this.current_user.number} ` + new Date().toUTCString());
             console.log('sorry, its only for users');
             return;
         }
@@ -53,32 +61,42 @@ const ATM = {
         amount = Number(amount);
 
         if (isNaN(amount) || amount < 1) {
+            this.history.push(`getCash: false, incorrect input ${this.current_type}: `
+                + `${this.current_user.number} ` + new Date().toUTCString());
             console.log('incorrect input');
             return;
         }
 
         if (amount > this.current_user.debet) {
+            this.history.push(`getCash: false, you not have so many money ${this.current_type}: `
+                + `${this.current_user.number} ` + new Date().toUTCString());
             console.log('you have only ' + this.current_user.debet + ' money');
             return;
         }
         if (amount > this.cash) {
-            console.log('ATM have only ' + this.cash + ' money');
+            this.history.push(`getCash: false, ATM not have so many money ${this.current_type}: `
+                + `${this.current_user.number} ` + new Date().toUTCString());
+            console.log(`ATM have only ${this.cash} money`);
             return;
         }
         this.cash -= amount;
         // noinspection JSPrimitiveTypeWrapperUsage
         this.current_user.debet -= amount;
-        console.log('you get ' + amount + ' money, your debet is ' + this.current_user.debet);
-        this.history = 'get Cash ' + this.current_type + ': ' + this.current_user.number + ' get ' + amount;
+        console.log(`you get ${amount} money, your debet is ${this.current_user.debet}`);
+        this.history.push(`getCash: true ${this.current_type}: ${this.current_user.number} get ${amount} `
+            + new Date().toUTCString());
     },
     // load cash - available for user only
     loadCash: function(amount){
         if (!this.is_auth) {
+            this.history.push('loadCash: false, not authorized ' + new Date().toUTCString());
             console.log('do authorization pls');
             return;
         }
 
         if (this.current_type === 'admin') {
+            this.history.push(`loadCash: false, it\`s not user ${this.current_type}: ${this.current_user.number} `
+                + new Date().toUTCString());
             console.log('sorry, its only for users');
             return;
         }
@@ -86,22 +104,27 @@ const ATM = {
         amount = Number(amount);
 
         if (isNaN(amount) || amount < 1) {
+            this.history.push(`loadCash: false, incorrect input ${this.current_type}: ${this.current_user.number} `
+                + new Date().toUTCString());
             console.log('incorrect input');
             return;
         }
         this.cash += amount;
         // noinspection JSPrimitiveTypeWrapperUsage
         this.current_user.debet += amount;
-        console.log('you load ' + amount + ' money, your debet is ' + this.current_user.debet);
-        this.history = 'load Cash ' + this.current_type + ': ' + this.current_user.number + ' load ' + amount;
+        console.log(`you load ${amount} money, your debet is ${this.current_user.debet}`);
+        this.history.push(`loadCash: true, ${this.current_type}: ${this.current_user.number} load ${amount} `
+           + new Date().toUTCString());
     },
     // load cash to ATM - available for admin only - EXTENDED
     load_cash: function(addition) {
         if (!this.is_auth) {
+            this.history.push('load_cash: false, not authorized ' + new Date().toUTCString());
             console.log('do authorization pls');
             return;
         }
         if (this.current_type !== 'admin') {
+            this.history.push('load_cash: false, not admin ' + new Date().toUTCString());
             console.log('access denied');
             return;
         }
@@ -110,34 +133,39 @@ const ATM = {
 
         if (isNaN(addition) || addition < 1) {
             console.log('incorrect input');
+            this.history.push('load_cash: false, incorrect input ' + new Date().toUTCString());
             return;
         }
 
         this.cash += addition;
         console.log('you load ' + addition + ' money in ATM, now ATM have ' + this.cash);
-        this.history = 'load cash in ATM ' + this.current_type + ': ' + this.current_user.number;
+        this.history.push(`load_cash: true, load cash in ATM ${this.current_type}: `
+            + `${this.current_user.number} load: ${addition} ` + new Date().toUTCString());
     },
     // get report about cash actions - available for admin only - EXTENDED
     getReport: function() {
         if (!this.is_auth) {
-            console.log('do authorization pls');
+            console.log('you not authorized');
+            this.history.push('getReport: false, not authorization ' + new Date().toUTCString());
             return;
         }
 
         if (this.current_type !== 'admin') {
             console.log('access denied');
+            this.history.push('getReport: false, not admin ' + new Date().toUTCString());
             return;
         }
-        console.log('cash: ' + this.cash + '\n' + 'Last operation: ' + this.history);
-        this.history = 'get Report ' + this.current_type + ': ' + this.current_user.number;
+        console.log(`cash: ${this.cash}\nLast operation: ${this.history[this.history.length - 1]}`);
+        this.history.push(`getReport: true, ${this.current_type}: ${this.current_user.number} ` + new Date().toUTCString());
     },
     // log out
     logout: function() {
         if (!this.is_auth) {
+            this.history.push('logout:  not a logged ' + new Date().toUTCString());
             console.log('you not a logged');
             return;
         }
-        this.history = 'logout ' + this.current_type + ': ' + this.current_user.number;
+        this.history.push(`logout: true ${this.current_type}: ${this.current_user.number} ` + new Date().toUTCString());
         console.log('Logout ok');
         this.is_auth = false;
         this.current_user = false;
