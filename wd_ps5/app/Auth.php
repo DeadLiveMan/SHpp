e88ad04b81853c32b70aecccd3859a96a166a21c
DeadLiveMan <deadliveman2@gmail.com>
2019-01-29 18:06:08 +0200

    add check server side for pass/login correct input

--- wd_ps5/app/Auth.php
+++ wd_ps5/app/Auth.php
@@ -6,6 +6,11 @@ class Auth
     private const PASSWORD_WRONG = 'Wrong password';
     private const CORRECT_RESPONSE = '0';
     private const SERVICE_ERROR = 'Service is temporarily unavailable';
+    private const LOGIN_INCORRECT = 'Incorrect login symbols';
+    private const PASSWORD_INCORRECT = 'Incorrect password symbols';
+
+    private const VALID_LOGIN = '/(^[a-zA-Z]+([a-zA-Z0-9][\s]?)*$)/';
+    private const VALID_PASSWORD = '/(^[a-zA-Z0-9]+$)/';
 
     public function __construct(IUsersDataBase $filePathUsers)
     {
@@ -14,6 +19,17 @@ class Auth
 
     public function login($login, $pass)
     {
+        $login = trim($login);
+        if (!preg_match(self::VALID_LOGIN, $login)) {
+            echo self::LOGIN_INCORRECT;
+            return;
+        }
+
+        if (!preg_match(self::VALID_PASSWORD, $pass)) {
+            echo self::PASSWORD_INCORRECT;
+            return;
+        }
+
         if(isset($login, $pass)) {
             if ($this->filePathUsers->checkUser($login)) {
                 echo $this->auth($login, $pass);
