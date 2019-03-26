983946431fb7e84bfb5522c2feed4685bc7c7996
DeadLiveMan <deadliveman2@gmail.com>
2019-03-26 18:01:03 +0200

    refactoring code

--- wd_ps4/task2/app/handler.php
+++ wd_ps4/task2/app/handler.php
@@ -1,15 +1,15 @@
 <?php
-require '..'.DIRECTORY_SEPARATOR.'app'.DIRECTORY_SEPARATOR.'readStatistics.php';
+require '..' . DIRECTORY_SEPARATOR . 'app' . DIRECTORY_SEPARATOR . 'readStatistics.php';
 
 $statistics = getStatistics();
 
 if (isset($_POST["vote"]) && array_key_exists($_POST["vote"], $statistics)) {
     $product = $_POST["vote"];
-    $fileName = '..'.DIRECTORY_SEPARATOR.'public'.DIRECTORY_SEPARATOR.'statistics.json';
+    $fileName = '..' . DIRECTORY_SEPARATOR . 'public' . DIRECTORY_SEPARATOR . 'statistics.json';
 
     $statistics -> $product++;
 
     file_put_contents($fileName, json_encode($statistics, JSON_PRETTY_PRINT));
 }
 
-header("location:..".DIRECTORY_SEPARATOR."public".DIRECTORY_SEPARATOR."piechart.php");
+header('location:..' . DIRECTORY_SEPARATOR . 'public' . DIRECTORY_SEPARATOR . 'piechart.php');
--- wd_ps4/task2/app/readStatistics.php
+++ wd_ps4/task2/app/readStatistics.php
@@ -1,24 +1,19 @@
 <?php
+
+define('FILE_NAME', '..'.DIRECTORY_SEPARATOR.'public'.DIRECTORY_SEPARATOR.'statistics.json');
+define('DEFAULT_STATISTIC', '
+        {   "Cactus": 0, 
+            "Beans": 0,
+            "Blackberry": 0,
+            "Cannabis": 0,
+            "Strawberry": 0
+        }'
+);
+
 function getStatistics()
 {
-    define('FILE_NAME', '..'.DIRECTORY_SEPARATOR.'public'.DIRECTORY_SEPARATOR.'statistics.json');
-    define('DEFAULT_STATISTIC',
-        [
-            'Cactus' => 0,
-            'Beans' => 0,
-            'Blackberry' => 0,
-            'Cannabis' => 0,
-            'Strawberry' => 0
-        ]
-    );
-
-    if (!file_exists(FILE_NAME)) {
-        file_put_contents(FILE_NAME, json_encode(DEFAULT_STATISTIC));
+    if (file_exists(FILE_NAME)) {
+        return json_decode(file_get_contents(FILE_NAME));
     }
-
-    $statistics = false;
-    if (filesize(FILE_NAME)) {
-        $statistics = json_decode(file_get_contents(FILE_NAME));
-    }
-    return $statistics;
+    return json_decode(DEFAULT_STATISTIC);
 }
