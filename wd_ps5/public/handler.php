d86d834361bb9ed625e8d0ab30369b67d24e9bea
DeadLiveMan <deadliveman2@gmail.com>
2019-05-31 11:49:18 +0300

    add earlt return in handler.php

--- wd_ps5/public/handler.php
+++ wd_ps5/public/handler.php
@@ -26,66 +26,68 @@ $messenger = new Messenger(new JsonMessagesDataBase($config['filePathChat']));
 $userHandler = new UserHandler(new JsonUsersDataBase($config['filePathUsers']));
 $validator = new Validator($errorLogs);
 
-if (isset($_POST['command'])) {
-    switch ($_POST['command']) {
-        case 'auth':
-            $login = $_POST['login'] ?? '';
-            $pass = $_POST['pass'] ?? '';
-            $login = trim($login);
+if (!isset($_POST['command'])) {
+    return;
+}
 
-            if (!$validator->isValid($login, $pass)) {
+switch ($_POST['command']) {
+    case 'auth':
+        $login = $_POST['login'] ?? '';
+        $pass = $_POST['pass'] ?? '';
+        $login = trim($login);
+
+        if (!$validator->isValid($login, $pass)) {
+            break;
+        }
+
+        if (!$userHandler->userExist($login)) {
+            if (!$userHandler->registration($login, $pass)) {
+                $errorLogs->setServerError('Error create new user');
                 break;
             }
-
-            if (!$userHandler->userExist($login)) {
-                if (!$userHandler->registration($login, $pass)) {
-                    $errorLogs->setServerError('Error create new user');
-                    break;
-                }
-            } else {
-                if (!$userHandler->authorize($login, $pass)) {
-                    $errorLogs->setErrorPassword('Wrong password');
-                    break;
-                }
+        } else {
+            if (!$userHandler->authorize($login, $pass)) {
+                $errorLogs->setErrorPassword('Wrong password');
+                break;
             }
+        }
 
-            $_SESSION['login'] = $login;
-            break;
-        case 'logout':
-            session_destroy();
-            break;
-        case 'send':
-            $login = $_SESSION['login'] ?? '';
-            $message = $_POST['message'] ?? '';
-            if ($login == '' || $message == '') { break; }
-            if (!$validator->checkMessageLength($message)) { break; }
-            $message = trim($message);
-            if (!$messenger->sendMessage($login, $message)) {
-                $errorLogs->setServerError('Message not send');
-            };
-            break;
-        case 'read':
-            $lastMessageTime = $_POST['lastTime'] ?? '';
-            $login = $_SESSION['login'] ?? '';
+        $_SESSION['login'] = $login;
+        break;
+    case 'logout':
+        session_destroy();
+        break;
+    case 'send':
+        $login = $_SESSION['login'] ?? '';
+        $message = $_POST['message'] ?? '';
+        if ($login == '' || $message == '') { break; }
+        if (!$validator->checkMessageLength($message)) { break; }
+        $message = trim($message);
+        if (!$messenger->sendMessage($login, $message)) {
+            $errorLogs->setServerError('Message not send');
+        };
+        break;
+    case 'read':
+        $lastMessageTime = $_POST['lastTime'] ?? '';
+        $login = $_SESSION['login'] ?? '';
 
-            if ($lastMessageTime !== '' && $login !== '') {
-                if (+$lastMessageTime === 0) {
-                    $lastMessageTime = mktime(
-                            date('H') - $config['timeForOldPosts'],
-                            date('i'),
-                            date('s'),
-                            date("m"),
-                            date("d"),
-                            date("Y")
-                        ) * 1000;
-                }
-                echo $messenger->readMessages($lastMessageTime);
-                return;
+        if ($lastMessageTime !== '' && $login !== '') {
+            if (+$lastMessageTime === 0) {
+                $lastMessageTime = mktime(
+                        date('H') - $config['timeForOldPosts'],
+                        date('i'),
+                        date('s'),
+                        date("m"),
+                        date("d"),
+                        date("Y")
+                    ) * 1000;
             }
-            break;
-        default:
-            $errorLogs->setServerError('Error command');
-            break;
-    }
-    echo(json_encode($errorLogs->getErrors()));
+            echo $messenger->readMessages($lastMessageTime);
+            return;
+        }
+        break;
+    default:
+        $errorLogs->setServerError('Error command');
+        break;
 }
+echo(json_encode($errorLogs->getErrors()));
