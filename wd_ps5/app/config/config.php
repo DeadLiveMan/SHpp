0430c99907d720076bcc5368466023db8dea2444
DeadLiveMan <deadliveman2@gmail.com>
2019-05-31 10:22:16 +0300

    add spaces in config

--- wd_ps5/app/config/config.php
+++ wd_ps5/app/config/config.php
@@ -1,12 +1,12 @@
 <?php
     define('ROOT_DIRECTORY', dirname(__DIR__, 2) . DIRECTORY_SEPARATOR);
     return [
-        'filePathUsers' => ROOT_DIRECTORY.'db'.DIRECTORY_SEPARATOR.'users.json',
-        'filePathChat' => ROOT_DIRECTORY.'db'.DIRECTORY_SEPARATOR.'chat.json',
+        'filePathUsers' => ROOT_DIRECTORY . 'db' . DIRECTORY_SEPARATOR . 'users.json',
+        'filePathChat' => ROOT_DIRECTORY . 'db' . DIRECTORY_SEPARATOR . 'chat.json',
 
-        'filePathLoginForm' => ROOT_DIRECTORY.'public'.DIRECTORY_SEPARATOR . 'login.php',
-        'filePathChatForm' => ROOT_DIRECTORY.'public'.DIRECTORY_SEPARATOR.'chat.php',
-        'filePathLogoutForm' => ROOT_DIRECTORY.'app'.DIRECTORY_SEPARATOR.'templates'.DIRECTORY_SEPARATOR.'logout.php',
+        'filePathLoginForm' => ROOT_DIRECTORY . 'public' . DIRECTORY_SEPARATOR . 'login.php',
+        'filePathChatForm' => ROOT_DIRECTORY . 'public' . DIRECTORY_SEPARATOR . 'chat.php',
+        'filePathLogoutForm' => ROOT_DIRECTORY . 'app' . DIRECTORY_SEPARATOR . 'templates' . DIRECTORY_SEPARATOR . 'logout.php',
 
         'maxLoginLength' => 18,
         'maxMessageLength' => 255,
