2e6a2f846f567a571ad13dde71aa8384626384a9
Thorus <deadliveman2@gmail.com>
2019-06-05 15:58:42 +0300

    refactoring regExp Marker, change style

--- wd_ps7/marker/public/js/script.js
+++ wd_ps7/marker/public/js/script.js
@@ -2,32 +2,71 @@ window.onload = function () {
 
     const textArea = document.getElementById('text-area');
     const regInput = document.getElementById('input-reg');
-
     const outputDiv = document.getElementById('output-text');
 
+    const flagsInfo = $('#flags-info h2');
+    const button = $('#btn');
+    const flagReg = $('.flag-reg');
+
+    let flags;
+    flagReg.on('click', function () {
+        flags = '';
+        flagReg.each(function () {
+            if(this.checked) {
+                flags += this.value;
+            }
+            flagsInfo[0].innerText = '/' + flags + '/';
+        });
+        checkReg();
+    });
+
+
     textArea.oninput = function () {
-        checkReg(textArea.value, regInput.value);
+        checkReg();
     };
 
     regInput.oninput = function () {
-        checkReg(textArea.value, regInput.value);
+        checkReg();
     };
 
-    function checkReg(text, reg) {
+    button.click(function(e) {
+        e.preventDefault();
+        checkReg();
+    });
 
-        const regularExp = reg.substr(1, reg.lastIndexOf('/') - 1);
-        const flags = reg.substr(reg.lastIndexOf('/') + 1);
+    function checkReg() {
+        if (!regInput.value) {
+            return;
+        }
 
-        const regExp = new RegExp(regularExp, flags);
+        const regExp = new RegExp(regInput.value, flags);
 
+        const mark  = {
+            open: '<mark>',
+            close: '</mark>'
+        };
 
-        // outputDiv.innerHTML = text.replace(regExp, function (findElement) {
-        //     return '<mark>' + findElement + '</mark>';
-        // });
+        const text = textArea.value;
 
+        let markedText = (text.replace(regExp, function (findElement) {
+            return mark.open + findElement + mark.close;
+        }));
 
-
+        $(outputDiv).html(markedText);
     }
 
+    function htmlEncode(value){
+        if (!value) {
+            return '';
+        }
+        return $('<div />').text(value).html();
+    }
+
+    function htmlDecode(value) {
+        if (!value) {
+            return '';
+        }
+        return $('<div />').html(value).text();
+    }
 
 };
